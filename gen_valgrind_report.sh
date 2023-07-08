642efd712fd73edf19c77d002f82f006b1184ab9
DesertCarMechanic <desertcarmechanic@gmail.com>
2023-07-08 13:36:26 +0300

    fixed valgrind report generation script

--- gen_valgrind_report.sh
+++ gen_valgrind_report.sh
@@ -1,1 +1,1 @@
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind_report ./test
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind_report ./compiled_test_suite
